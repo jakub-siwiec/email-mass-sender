42edea371625dc1abf0632beebf35c52b8b9a6c1
jakub-siwiec <jakub.siwiec@svcais.com>
2020-09-29 19:05:23 +0100

    Small fixes in main.py

--- assets/htmltemplates/README.md
+++ assets/htmltemplates/README.md
@@ -1,3 +1,5 @@
 # Temmplate files
 
-The folder for two separate templates: for email title and email body. Their names should match `EMAIL_TITLE_FILE_NAME` and `EMAIL_BODY_FILE_NAME` from `.env` file. These files should be in HTML format.
+The folder for two separate templates: for email title and email body. Their names should match `EMAIL_TITLE_FILE_NAME` and `EMAIL_BODY_FILE_NAME` from *.env* file. These files should be in HTML format.
+
+The variables should be enclosed in curly braces {}.
--- main.py
+++ main.py
@@ -7,45 +7,54 @@ from subfiles.emailtemplateextractor import Emailtemplate
 from subfiles.emailfinder import Emailfinder
 from subfiles.bulksend import Bulksending
 
-# Confirmation for sending emails
-input_value = input(
-    "\nPlease double check the correctness of Excel spreadsheet and email template.\nAre you sure you want to continue (Y/N)?\n")
 
-if (input_value == "Y" or input_value == "y"):
-    # Path to env keys
-    PATH_ENV = Path(Path.cwd(), "envkeysfiles", ".env")
-    # Loading enviornment variables
-    load_dotenv(dotenv_path=PATH_ENV)
+def main():
+    # Confirmation for sending emails
+    input_value = input(
+        "\nPlease double check the correctness of Excel spreadsheet and email template.\nAre you sure you want to continue (Y/N)?\n")
 
-    # Other path constants
-    PATH_XLSX = Path(Path.cwd(), "assets", "spreadsheet",
-                     os.getenv("DATABASE_FILE_NAME"))
-    PATH_EMAIL_TITLE = Path(Path.cwd(), "assets",
-                            "htmltemplates", os.getenv("EMAIL_TITLE_FILE_NAME"))
-    PATH_EMAIL_BODY = Path(Path.cwd(), "assets",
-                           "htmltemplates", os.getenv("EMAIL_BODY_FILE_NAME"))
-    PATH_EMAIL_ATTACHMENT = Path(Path.cwd(), "assets",
-                                 "attachments", os.getenv("ATTACHMENT_FILE_NAME"))
+    print("\n")
 
-    # Loading data from .xlsx file
-    xlsx_file = Spreadsheet(PATH_XLSX)
+    if (input_value == "Y" or input_value == "y"):
+        # Path to env keys
+        PATH_ENV = Path(Path.cwd(), "envkeysfiles", ".env")
+        # Loading enviornment variables
+        load_dotenv(dotenv_path=PATH_ENV)
 
-    # Loading template from HTML file
-    email_template = Emailtemplate(
-        PATH_EMAIL_TITLE, PATH_EMAIL_BODY)
+        # Other path constants
+        PATH_XLSX = Path(Path.cwd(), "assets", "spreadsheet",
+                         os.getenv("DATABASE_FILE_NAME"))
+        PATH_EMAIL_TITLE = Path(Path.cwd(), "assets",
+                                "htmltemplates", os.getenv("EMAIL_TITLE_FILE_NAME"))
+        PATH_EMAIL_BODY = Path(Path.cwd(), "assets",
+                               "htmltemplates", os.getenv("EMAIL_BODY_FILE_NAME"))
+        PATH_EMAIL_ATTACHMENT = Path(Path.cwd(), "assets",
+                                     "attachments", os.getenv("ATTACHMENT_FILE_NAME"))
 
-    # Loading email address finding details
-    email_finder = Emailfinder(str(os.getenv("HUNTER_API_KEY")))
+        # Loading data from .xlsx file
+        xlsx_file = Spreadsheet(PATH_XLSX)
 
-    # Load sending data emails
-    gmail_send = Gmailsend(os.getenv("GMAIL_EMAIL"), os.getenv("GMAIL_ALIAS"))
+        # Loading template from HTML file
+        email_template = Emailtemplate(
+            PATH_EMAIL_TITLE, PATH_EMAIL_BODY)
 
-    # Loading bulk send for sending emails
-    bulk_send = Bulksending(xlsx_file, gmail_send, email_finder,
-                            email_template, str(PATH_EMAIL_ATTACHMENT))
+        # Loading email address finding details
+        email_finder = Emailfinder(str(os.getenv("HUNTER_API_KEY")))
 
-    # Sending emails
-    bulk_send.sendmail()
-    print("\nEmail sending completed.\n")
-else:
-    print("\nDid not send any email\n")
+        # Load sending data emails
+        gmail_send = Gmailsend(os.getenv("GMAIL_EMAIL"),
+                               os.getenv("GMAIL_ALIAS"))
+
+        # Loading bulk send for sending emails
+        bulk_send = Bulksending(xlsx_file, gmail_send, email_finder,
+                                email_template, str(PATH_EMAIL_ATTACHMENT))
+
+        # Sending emails
+        bulk_send.sendmail()
+        print("\nEmail sending completed.\n")
+    else:
+        print("\nDid not send any email\n")
+
+
+if __name__ == '__main__':
+    main()
